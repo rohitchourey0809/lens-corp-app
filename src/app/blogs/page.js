4595a062650bbed2791ce73c1e26597f1b7eb228
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2024-05-11 01:09:47 +0530

    Blog component added

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
-  )
-}
+    <section className="ourblogs" id="section-8">
+      <div className="flex justify-between gap-8 p-8">
+        <div className="bg-gray-600 p-10 rounded-lg hover:border-2 hover:border-green-500">
+          <div>
+            <h2 className="text-red-500">New!</h2>
+          </div>
+          <div>
+            <h1 className="font-bold">Generative AI</h1>
+          </div>
+          <div>
+            <p className="text-black">
+              In todays data-driven marketing world, keeping up with content
+              creation demands can be a challenge. Read our latest blog to
+              explore the power of Generative AI and its potential to reshape
+              the marketing landscape....
+            </p>
+          </div>
+        </div>
+        <div className="bg-gray-600 p-10 rounded-lg hover:border-2 hover:border-green-500">
+          <div>
+            <h2 className="text-red-500">New!</h2>
+          </div>
+          <div>
+            <h1 className="font-bold">Generative AI</h1>
+          </div>
+          <div>
+            <p className="text-black">
+              In todays data-driven marketing world, keeping up with content
+              creation demands can be a challenge. Read our latest blog to
+              explore the power of Generative AI and its potential to reshape
+              the marketing landscape....
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Blogs
+export default Blogs;
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import navimage from "../../public/nav_logo.webp";
 
 import Link from "next/link";
 import About from "./about/page";
+import Blogs from "./blogs/page";
 
 export default function Home() {
   return (
@@ -384,42 +385,7 @@ export default function Home() {
 
       <section className="4divcontainer" id="section-6"></section>
       <section className="ourvision" id="section-7"></section>
-      <section className="ourblogs" id="section-8">
-        <div className="flex justify-between gap-8 p-8">
-          <div className="bg-gray-600 p-10 rounded-lg hover:border-2 hover:border-green-500">
-            <div>
-              <h2 className="text-red-500">New!</h2>
-            </div>
-            <div>
-              <h1 className="font-bold">Generative AI</h1>
-            </div>
-            <div>
-              <p className="text-black">
-                In todays data-driven marketing world, keeping up with content
-                creation demands can be a challenge. Read our latest blog to
-                explore the power of Generative AI and its potential to reshape
-                the marketing landscape....
-              </p>
-            </div>
-          </div>
-          <div className="bg-gray-600 p-10 rounded-lg hover:border-2 hover:border-green-500">
-            <div>
-              <h2 className="text-red-500">New!</h2>
-            </div>
-            <div>
-              <h1 className="font-bold">Generative AI</h1>
-            </div>
-            <div>
-              <p className="text-black">
-                In todays data-driven marketing world, keeping up with content
-                creation demands can be a challenge. Read our latest blog to
-                explore the power of Generative AI and its potential to reshape
-                the marketing landscape....
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Blogs/>
 
       <section className="weworkclient" id="section-8"></section>
       <section className="Bynumber" id="section-8"></section>
